9bd63864251edc98268292d5b36ccb729f442702
Corosauce <cordonfreeman@gmail.com>
2018-11-21 08:14:35 -0500

    Version bump, fix version string.

--- src/main/java/com/corosus/desirepaths/DesirePaths.java
+++ src/main/java/com/corosus/desirepaths/DesirePaths.java
@@ -1,8 +1,5 @@
 package com.corosus.desirepaths;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.minecraft.block.Block;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class DesirePaths
 {
     public static final String MODID = "desirepaths";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "${version}";
     
     public static Block dirt_6; //fully worn (our custom dirt)
     public static Block dirt_5;
